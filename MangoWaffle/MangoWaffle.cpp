ffedf3451ab0321e475772cde17975250e55142c
동재 성 <flint21c1996@gmail.com>
2025-04-01 17:36:57 +0900

    <방문 길이>/Lv 2/62%

--- MangoWaffle/MangoWaffle.cpp
+++ MangoWaffle/MangoWaffle.cpp
@@ -1,62 +1,95 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include<tuple>
-
+#include <iostream>
 using namespace std;
 
-bool compare(tuple<int, float> a, tuple<int, float> b)
-{
-	if (get<1>(a) == get<1>(b))
+//이차원 배열을 통해 풀어보자.
+//배열을 할당받고, 캐릭터가 걷는길의 배열을 바꾸자.
+
+int _x[4] = { 0,0,-1,1 }; //상,하,좌,우
+int _y[4] = { 1,-1,0,0 };
+
+int solution(string dirs) {
+	int answer = 0;
+	//가로세로 0~10까지의 이중배열을 만들자.
+	bool path[11][11][4];
+	pair<int, int> pos = { 5,5 };
+	for (int i = 0; i < dirs.size(); i++)
 	{
-		return get<0>(a) < get<0>(b);
-	}
-	return get<1>(a) > get<1>(b);
-}
-//N = 전체 스테이지의 개수, stages 사용자가 현재 멈춰있는 스테이지 1~N+1 / N+1의 경우 전체 다깸
-vector<int> solution(int N, vector<int> stages)
-{
-	vector<int> answer;
-	vector<int> peopleStop(N, 0);
-	vector<int> peopleStage(N, 0);
-	vector<tuple<int, float>> result(N);
-	for (int i = 0; i < stages.size(); i++)
-	{
-		//스테이지를 클리어한 사람
-		for (int j = 0; j < stages[i] - 1; j++)
+		pair<int, int> tempPos = pos;
+		//string값에 따라 tempPos값에 특정인자를 더하고 빼준다.
+		if (dirs[i] == 'U')
 		{
-			peopleStage[j]++;
+			tempPos.first += _x[0];
+			tempPos.second += _y[0];
+		};
+		if (dirs[i] == 'D')
+		{
+			tempPos.first += _x[1];
+			tempPos.second += _y[1];
+		};
+		if (dirs[i] == 'L')
+		{
+			tempPos.first += _x[2];
+			tempPos.second += _y[2];
+		};
+		if (dirs[i] == 'R')
+		{
+			tempPos.first += _x[3];
+			tempPos.second += _y[3];
+		};
+
+		//만약 포지션이 범위를 벗어난다면 continue한다.
+		if (tempPos.first < 0 || tempPos.first>10 || tempPos.second < 0 || tempPos.second>10)
+		{
+			continue;
+		}
+		//벗어나지 않았을경우 배열의 3차원 공간의 값을 바꿔준다.
+		if (dirs[i] == 'U')
+		{
+			path[tempPos.first][tempPos.second][1] = false;
+			path[pos.first][pos.second][0] = false;
+		}
+		if (dirs[i] == 'D')
+		{
+			path[tempPos.first][tempPos.second][0] = false;
+			path[pos.first][pos.second][1] = false;
+		}
+		if (dirs[i] == 'L')
+		{
+			path[tempPos.first][tempPos.second][3] = false;
+			path[pos.first][pos.second][2] = false;
+		}
+		if (dirs[i] == 'R')
+		{
+			path[tempPos.first][tempPos.second][2] = false;
+			path[pos.first][pos.second][3] = false;
 		}
 
-		//스테이지에 멈춰있는 사람
-		if (stages[i] != N + 1)
+		pos = tempPos;
+	}
+
+	//path배열의 전체를 순환하면서 true의 값을 저장하고 2로 나눈다.
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 11; j++)
 		{
-			peopleStop[stages[i]-1]++;
+			for (int k = 0; k < 4; k++)
+			{
+				if (path[i][j][k] == false)
+				{
+					answer++;
+				}
+			}
 		}
 	}
-
-	for (int i = 0; i < result.size(); i++)
-	{
-		if ((peopleStop[i] + peopleStage[i]) == 0)
-		{
-			result[i] = { i,0 };
-		}
-		else
-			result[i] = { i, static_cast<float>(peopleStop[i]) / (peopleStop[i] + peopleStage[i]) };
-	}
-	sort(result.begin(), result.end(), compare);
-
-	for (int i = 0; i < result.size(); i++)
-	{
-		answer.push_back(get<0>(result[i])+1);
-	}
-
+	answer /= 2;
 	return answer;
 }
+
 int main()
 {
-	int n = 5;
-	vector<int> a = { 2, 1, 2, 6, 2, 4, 3, 3 };
-	auto e = solution(5, a);
-	return 0;
+	string a = "LULLLLLLU";
+	int b = solution(a);
+	std::cout << b;
 }
--- 망고와플/arrayCodingTest.cpp
+++ 망고와플/arrayCodingTest.cpp
@@ -185,8 +185,103 @@ int main()
 	auto e = solution(5, a);
 	return 0;
 }
-//---------------------------------------------------------------------------------
+//------------------------------------방문 길이--------------------------------------
+//아래의 문제는 한번 더 풀어보면 좋을듯하다. >> 조금 더 최적화 할수 있으면 좋았을껄
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
 
+//이차원 배열을 통해 풀어보자.
+//배열을 할당받고, 캐릭터가 걷는길의 배열을 바꾸자.
+
+int _x[4] = { 0,0,-1,1 }; //상,하,좌,우
+int _y[4] = { 1,-1,0,0 };
+
+int solution(string dirs) {
+	int answer = 0;
+	//가로세로 0~10까지의 이중배열을 만들자.
+	bool path[11][11][4];
+	pair<int, int> pos = { 5,5 };
+	for (int i = 0; i < dirs.size(); i++)
+	{
+		pair<int, int> tempPos = pos;
+		//string값에 따라 tempPos값에 특정인자를 더하고 빼준다.
+		if (dirs[i] == 'U')
+		{
+			tempPos.first += _x[0];
+			tempPos.second += _y[0];
+		};
+		if (dirs[i] == 'D')
+		{
+			tempPos.first += _x[1];
+			tempPos.second += _y[1];
+		};
+		if (dirs[i] == 'L')
+		{
+			tempPos.first += _x[2];
+			tempPos.second += _y[2];
+		};
+		if (dirs[i] == 'R')
+		{
+			tempPos.first += _x[3];
+			tempPos.second += _y[3];
+		};
+
+		//만약 포지션이 범위를 벗어난다면 continue한다.
+		if (tempPos.first < 0 || tempPos.first>10 || tempPos.second < 0 || tempPos.second>10)
+		{
+			continue;
+		}
+		//벗어나지 않았을경우 배열의 3차원 공간의 값을 바꿔준다.
+		if (dirs[i] == 'U')
+		{
+			path[tempPos.first][tempPos.second][1] = false;
+			path[pos.first][pos.second][0] = false;
+		}
+		if (dirs[i] == 'D')
+		{
+			path[tempPos.first][tempPos.second][0] = false;
+			path[pos.first][pos.second][1] = false;
+		}
+		if (dirs[i] == 'L')
+		{
+			path[tempPos.first][tempPos.second][3] = false;
+			path[pos.first][pos.second][2] = false;
+		}
+		if (dirs[i] == 'R')
+		{
+			path[tempPos.first][tempPos.second][2] = false;
+			path[pos.first][pos.second][3] = false;
+		}
+
+		pos = tempPos;
+	}
+
+	//path배열의 전체를 순환하면서 true의 값을 저장하고 2로 나눈다.
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 11; j++)
+		{
+			for (int k = 0; k < 4; k++)
+			{
+				if (path[i][j][k] == false)
+				{
+					answer++;
+				}
+			}
+		}
+	}
+	answer /= 2;
+	return answer;
+}
+
+int main()
+{
+	string a = "LULLLLLLU";
+	int b = solution(a);
+	std::cout << b;
+}
 //---------------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------------
